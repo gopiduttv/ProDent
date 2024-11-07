c7ec94223c62e62d74bcc0006a34e69f8b1b0b4e
Gopi Dutt V <gopiduttv@carestack.com>
2024-11-07 11:58:08 +0530

    ADD Layout component

--- src/components/common/Content.tsx
+++ src/components/common/Content.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Content() {
   return (
-    <div className="w-full h-3/4 bg-violet-800">
-      <div className="text-white text-9xl absolute z-50 w-full align-middle bg-violet-600 "></div>
+    <div className="w-full h-3/4 bg-red-800">
+      <div className="text-white text-9xl absolute z-50 w-full align-middle bg-red-600 ">CONTENT </div>
     </div>
   )
 }
