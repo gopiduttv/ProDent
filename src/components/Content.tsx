75109101c3ad930ebc33f6821a098e883852d3bc
Aravind C P <aravindcp@carestack.com>
2024-11-12 13:24:54 +0000

    Merged in Cms/sanitySchemeaSetup (pull request #5)
    Cms/sanitySchemeaSetup
    
    * sanity setup for OS Dental :sparkles:
    
    * sanity setup for OS Dental :sparkles:
    
    * site settings configuration updated
    
    * sanity API integration :tada:
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/osdental-website into Cms/sanitySchemeaSetup
    
    * sanity API integration :tada:
    
    
    Approved-by: Akash A S

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,15 +4,15 @@ import FeatureSection from './FeatureSection'
 import VisionSection from './VisionSection'
 import ColoredSection from './ColoredSection'
 
-const Content = () => {
+const Content = (props: any) => {
   return (
     <div className="flex flex-col overflow-hidden">
       <HeroSection />
       <FeatureSection />
-      <div className='bg-yellow-400 text-black'>INTEGRATIONS</div>
+      <div className="bg-yellow-400 text-black">INTEGRATIONS</div>
       <BenefitsSection />
-      <ColoredSection/>
-      <VisionSection/>
+      <ColoredSection />
+      <VisionSection />
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,36 @@ import { useLiveQuery } from 'next-sanity/preview'
 import Content from '~/components/Content'
 import { readToken } from '~/lib/sanity.api'
 import { getClient } from '~/lib/sanity.client'
-import { getPosts, type Post, postsQuery } from '~/lib/sanity.queries'
+import {
+  getPosts,
+  heroSectionQuery,
+  metaDataQuery,
+  type Post,
+  postsQuery,
+  SiteSettings,
+} from '~/lib/sanity.queries'
 import type { SharedPageProps } from '~/pages/_app'
 import Layout from '../components/Layout'
+import CustomHead from '~/components/common/CustomHead'
 
 export const getStaticProps: GetStaticProps<
   SharedPageProps & {
     posts: Post[]
+    siteSettings: SiteSettings
   }
 > = async ({ draftMode = false }) => {
   const client = getClient(draftMode ? { token: readToken } : undefined)
   const posts = await getPosts(client)
+  const siteSettings = await metaDataQuery(client)
+  const heroSectionContent = await heroSectionQuery(client)
 
   return {
     props: {
       draftMode,
       token: draftMode ? readToken : '',
       posts,
+      siteSettings,
+      heroSectionContent,
     },
   }
 }
@@ -27,12 +40,14 @@ export const getStaticProps: GetStaticProps<
 export default function IndexPage(
   props: InferGetStaticPropsType<typeof getStaticProps>,
 ) {
-  const [posts] = useLiveQuery<Post[]>(props.posts, postsQuery)
+  // const [posts] = useLiveQuery<Post[]>(props.posts, postsQuery)
+  const siteSettings: SiteSettings = props?.siteSettings
   return (
-    <>
+    <div>
       <Layout>
-        <Content />
+      <CustomHead siteSettings={siteSettings} />
+        <Content {...props} />
       </Layout>
-    </>
+    </div>
   )
 }
