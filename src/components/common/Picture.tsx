ab863b470cb7eb066ddd2a2817396ec3665a71ba
Gopi Dutt V <gopiduttv@carestack.com>
2024-11-12 13:05:32 +0530

    ADD Picture component

---
+++ src/components/common/Picture.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Picture = (props) => {
+  return (
+    <div className="md:w-1/2 w-full flex ">
+      <div className="rounded-2xl overflow-hidden bg-stone-300 w-full">
+        <img
+          className={`object-cover w-full h-[${props.height ? props.height : 300}px]`}
+          src="/edgedummy.png"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Picture
