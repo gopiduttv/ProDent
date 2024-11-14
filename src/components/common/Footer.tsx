8d67d1ea5e39b756ccc273433816c4fe8985f599
Gopi Dutt V <gopiduttv@carestack.com>
2024-11-14 13:29:15 +0530

    ADD make footer responsive

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,13 +3,13 @@ import Section from '../structure/Section'
 
 const TopSection = () => {
   return (
-    <div className="self-stretch h-[205px] flex-col justify-start items-start gap-12 flex">
-      <div className="w-[1240px] justify-start items-start gap-24 inline-flex">
-        <div className="w-[593px] text-white text-5xl font-semibold leading-[57.60px]">
+    <div className="self-stretch flex-col justify-start items-start gap-12 flex">
+      <div className="w-full justify-start items-start gap-24 inline-flex flex-col md:flex-row">
+        <div className="w-1/2 text-white text-5xl font-semibold leading-[57.60px]">
           Centralize your data. Supercharge your DSO.
         </div>
-        <div className="grow shrink basis-0 flex-col justify-start items-start gap-[27px] inline-flex">
-          <div className="self-stretch h-[87px] flex-col justify-start items-start gap-3 flex">
+        <div className="grow shrink basis-0 flex-col justify-start items-start gap-2 inline-flex">
+          <div className="self-stretch  flex-col justify-start items-start gap-3 flex">
             <div className="self-stretch text-white/80 text-lg font-medium leading-[29.25px]">
               Without cross-platform compatibility, what good is your data? Our
               integrations are numerous, validated, and ready right now. Not
@@ -17,7 +17,10 @@ const TopSection = () => {
             </div>
           </div>
           <div className="px-4 py-2.5 bg-[#8639f8] rounded-md justify-center items-center gap-2.5 inline-flex">
-            <a href="/cta" className="text-white text-sm font-semibold leading-snug">
+            <a
+              href="/cta"
+              className="text-white text-sm font-semibold leading-snug"
+            >
               Book Free Demo
             </a>
           </div>
@@ -30,25 +33,35 @@ const TopSection = () => {
 
 const MiddleSection = () => {
   return (
-    <div className="self-stretch justify-start items-start gap-24 inline-flex">
-      <div className="grow shrink basis-0 flex-col justify-start items-start gap-3 inline-flex">
+    <div className="self-stretch justify-start items-start gap-12 inline-flex flex-col md:flex-row">
+      <div className="w-full grow shrink basis-0 flex-col gap-3 inline-flex">
         <div className="w-full h-8 relative">
           <img src="/OSDentalLogo.svg" />
         </div>
-        <div className="self-stretch text-white/70 text-base font-medium leading-relaxed">
+        <div className="self-stretch text-white/70 text-base font-medium leading-relaxed inline-flex">
           One Stop Dental Analytics
         </div>
       </div>
-      <div className="self-stretch flex-col justify-start gap-6 inline-flex">
-        <div className="text-white/80 text-sm font-light leading-snug">
-          300 Delaware Avenue <br/> Wilmington, Delaware 19801
+      <div className="self-stretch flex flex-col justify-center gap-6 ">
+        <div className="text-white/80 text-sm font-light flex justify-center md:justify-start ">
+          300 Delaware Avenue <br /> Wilmington, Delaware 19801
         </div>
-        <div className="justify-end items-start gap-6 inline-flex">
-          <div className="w-5 h-5 relative"><img src="/linkedin.svg" /></div>
-          <div className="w-5 h-5 relative"><img src="/facebook.svg" /></div>
-          <div className="w-5 h-5 relative"><img src="/instagram.svg" /></div>
-          <div className="w-5 h-5 relative"><img src="/youtube.svg" /></div>
-          <div className="w-5 h-5 relative"><img src="/vimeo.svg" /></div>
+        <div className="flex justify-center md:justify-start items-start gap-6 ">
+          <div className="w-5 h-5 relative">
+            <img src="/linkedin.svg" />
+          </div>
+          <div className="w-5 h-5 relative">
+            <img src="/facebook.svg" />
+          </div>
+          <div className="w-5 h-5 relative">
+            <img src="/instagram.svg" />
+          </div>
+          <div className="w-5 h-5 relative">
+            <img src="/youtube.svg" />
+          </div>
+          <div className="w-5 h-5 relative">
+            <img src="/vimeo.svg" />
+          </div>
         </div>
       </div>
     </div>
@@ -57,14 +70,14 @@ const MiddleSection = () => {
 
 const BottomSection = () => {
   return (
-    <div className="self-stretch h-14 flex-col justify-start items-start gap-8 flex">
+    <div className="self-stretch flex justify-start items-start gap-8 md:gap-0">
       <div className="self-stretch h-[0px] border border-white/20"></div>
-      <div className="self-stretch justify-between items-center inline-flex">
+      <div className="w-full self-stretch flex flex-col-reverse md:flex-row justify-between items-center gap-6">
         <div className="text-white/50">© 2023 Aeka. All Rights Reserved.</div>
-        <div className="justify-start items-center gap-6 flex">
-          <div className="text-right text-white/50  ">Terms of use</div>
-          <div className="text-right text-white/50 ">Privacy policy</div>
-          <div className="text-right text-white/50 ">Contact Us</div>
+        <div className="gap-8 md:gap flex flex-col md:flex-row ">
+          <div className=" text-white/50 ">Terms of use</div>
+          <div className=" text-white/50 ">Privacy policy</div>
+          <div className=" text-white/50 ">Contact Us</div>
         </div>
       </div>
     </div>
@@ -74,8 +87,8 @@ const BottomSection = () => {
 const Footer = () => {
   return (
     <Section id="join-waitlist" className={'bg-[#02024a]'}>
-      <Container className="justify-center py-4 text-white">
-        <div className="h-[575px] px-[244px] pt-24 pb-8 bg-[#02024a] flex-col justify-center items-start gap-12 inline-flex">
+      <Container className="flex justify-center py-4 text-white ">
+        <div className=" pt-24 pb-8 bg-[#02024a] flex-col justify-center items-start gap-12 inline-flex ">
           <TopSection />
           <MiddleSection />
           <BottomSection />
