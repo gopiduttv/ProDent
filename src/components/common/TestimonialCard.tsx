02aa47c823c162a25c0c6c6f095de9c6bf1b8897
Gopi Dutt V <gopiduttv@carestack.com>
2024-11-15 15:59:53 +0530

    Fix testimonial responsiveness

--- src/components/common/TestimonialCard.tsx
+++ src/components/common/TestimonialCard.tsx
@@ -1,10 +1,9 @@
-import testimonial from '~/schemas/testimonial'
 import H3 from '../typography/H3'
 import RichAvatar from './RichAvatar'
 
 const TestimonialCard = (props) => {
   return (
-      <div className="rounded-3xl bg-[#f9f6fe] p-10 flex flex-col gap-4 w-3/4 ">
+    <div className="rounded-3xl bg-[#f9f6fe] p-10 flex flex-col gap-4 w-[380px] md:w-3/4  ">
       <div className="flex items-center justify-between">
         <H3 className="text-lg font-semibold text-primary">{props.title}</H3>
       </div>
@@ -14,11 +13,9 @@ const TestimonialCard = (props) => {
         </p>
       </div>
       <div className="flex flex-row justify-between items-end">
-        <RichAvatar user={props.user} tenant={props.tenant}  />
+        <RichAvatar user={props.user} tenant={props.tenant} />
         <div>
-          <p className="text-md text-gray-900">
-            {props.infoString}
-          </p>
+          <p className="text-md text-gray-900">{props.infoString}</p>
         </div>
       </div>
     </div>
