3ffcaf85ae6cb2c9283f97b24e39c08efe8f517e
Gopi Dutt V <gopiduttv@carestack.com>
2024-11-17 09:41:05 +0530

    Fix touch up login button

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoginButton = () => {
   return (
-    <div className="h-10 px-4 py-2 border-2 border-[#8639f8] rounded-md justify-center items-center gap-2.5 inline-flex">
+    <div className="h-10 px-4 py-2 border-[1px] border-[#8639f8] rounded-md justify-center items-center gap-2.5 inline-flex">
       <a href="/login" className="text-[#8639f8] text-sm font-medium leading-snug">
         Login
       </a>
