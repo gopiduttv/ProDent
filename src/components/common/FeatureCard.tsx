4267643090f0090f7d3861d8ad37420860631155
Aravind_Cp <aravindcp@carestack.com>
2024-11-18 14:04:04 +0530

    :sparkles: OS Dental sanity Integration

--- src/components/common/FeatureCard.tsx
+++ src/components/common/FeatureCard.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import H3 from '../typography/H3'
-import Image from 'next/image'
 import List from './List'
+import Image from 'next/image'
 
-const FeatureCategoryCard = ({ featureCategory }) => {
+const FeatureCategoryCard = ({ feature }) => {
   return (
     <div className="flex flex-col w-full gap-4 bg-[#f9f6fe] p-8 rounded-3xl">
       <Image
-        src={featureCategory?.imageUrl}
+        src={feature?.imageUrl}
         width={50}
         height={50}
         alt="location-icon"
       />
-      <H3>{featureCategory?.heading}</H3>
-      <List className="flex flex-col gap-4" items={featureCategory.features.map(feature => feature.heading)}/>
+      <H3>{feature?.featureSubCategoriesHeading}</H3>
+      <List className="flex flex-col gap-4" items={feature} />
     </div>
   )
 }
