c2ad49a5cee67d96b06f6aa9f464be2b4ab75dbb
Aravind C P <aravindcp@carestack.com>
2024-11-20 09:10:22 +0000

    Merged in Cms/sanitySchemeaSetup (pull request #37)
    section heading data binding from sanity
    
    * section heading data binding from sanity
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/osdental-website into Cms/sanitySchemeaSetup

--- src/components/BenefitsSection.tsx
+++ src/components/BenefitsSection.tsx
@@ -4,15 +4,17 @@ import Container from './structure/Container'
 import RichImage from './common/RichImage'
 import H2 from './typography/H2'
 import Subtext from './typography/Subtext'
+import { useHomeContext } from './context/homeSettings'
 
 const BenefitsSection = ({ data }) => {
+  const benifitHeader = useHomeContext()?.homeSettings 
   return (
     <Section id="benefits-section" className="py-24 bg-purple-50">
       <Container className="flex flex-col items-center gap-16">
         <div className="flex flex-col w-full items-center gap-2">
           <Subtext>Benefits</Subtext>
           <div className="max-w-2xl w-full flex justify-center">
-            <H2>Do More With Your Data</H2>
+            <H2>{benifitHeader?.heroSectionData?.benefitHeader}</H2>
           </div>
         </div>
         <div className="flex flex-col gap-16 w-full">
