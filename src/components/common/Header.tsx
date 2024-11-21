c84300f67b74c27fa25e641be495a41d3669d422
Gopi Dutt V <gopiduttv@carestack.com>
2024-11-21 14:04:50 +0530

    Fix make some things more responsive

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -39,25 +39,24 @@ const Header = () => {
   return (
     <header
       ref={headerRef}
-      className="fixed  top-0 left-0 w-full h-[75px] py-4 px-4 backdrop-blur-sm  transition-colors duration-300 z-50"
-      
+      className="fixed  top-0 left-0 w-full h-[75px] py-4 backdrop-blur-sm  transition-colors duration-300 z-50"
     >
-        <nav className=" flex flex-col md:flex-row md:items-center md:justify-between gap-42 md:px-4">
-          <div className="flex flex-row justify-between px-4 md:px-0">
-            <Link href="/" className="flex items-center space-x-2">
-              <span className="font-bold text-lg">
-                <Image
-                  src="/OSDentalLogo.svg"
-                  width={135}
-                  height={26}
-                  alt="OS Dental Logo"
-                />
-              </span>
-            </Link>
-            <MobileMenuButton className="md:hidden p-2 border-0 focus:outline-none text-white" />
-          </div>
-          <Navbar ctaName={headerContent.ctaName} />
-        </nav>
+      <nav className=" flex flex-col xl:flex-row xl:items-center xl:justify-between gap-42 xl:px-4">
+        <div className="flex flex-row justify-between px-4 xl:px-0">
+          <Link href="/" className="flex items-center space-x-2">
+            <span className="font-bold text-lg">
+              <Image
+                src="/OSDentalLogo.svg"
+                width={135}
+                height={26}
+                alt="OS Dental Logo"
+              />
+            </span>
+          </Link>
+          <MobileMenuButton className="xl:hidden p-2 border-0 focus:outline-none text-white" />
+        </div>
+        <Navbar ctaName={headerContent.ctaName} />
+      </nav>
     </header>
   )
 }
