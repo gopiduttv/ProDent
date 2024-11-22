c0a45ec890e9440f2a3b948938c68c34e7d84d2f
Aravind_Cp <aravindcp@carestack.com>
2024-11-22 18:36:29 +0530

    fix: build error fix

--- src/components/PartnerSection.tsx
+++ src/components/PartnerSection.tsx
@@ -7,21 +7,16 @@ import Container from './structure/Container'
 export default function PartnerSection({ partnerList }) {
   const data = useHomeContext()?.homeSettings
   return (
-    <Section
-      id="partners-section"
-      className="py-8 bg-gradient-to-r from-[#8639f8]/50 to-[#f768d1]/50 flex-col justify-start items-center gap-6 "
-    >
+    <Section id="integrations-section" className="py-8 bg-gradient-to-r from-[#8639f8]/50 to-[#f768d1]/50 flex-col justify-start items-center gap-6 ">
       <Container className="flex flex-col items-center gap-16">
         <div className="w-full flex flex-col xl:flex-row justify-center items-center gap-6 ">
           {partnerList?.map((partner, index) => {
             return (
-              <div key={index} >
-                <ImageLoader
-                  className="flex justify-center h-20 w-72 bg-white rounded-lg "
-                  imageClassName="!object-contain h-20 w-auto"
-                  image={partner.image}
-                  fixed={false}
-                />
+              <div
+                key={index}
+                className="h-20 px-[17.30px] py-[10.81px] bg-white rounded-[10px] flex-col justify-center items-center gap-[10.81px] inline-flex"
+              >
+                <ImageLoader className="!w-20 !h-20" image={partner.image} />
               </div>
             )
           })}
