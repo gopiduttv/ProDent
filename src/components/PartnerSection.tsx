c694b8c27408a568fca9ec456bc518e59790c7bd
Gopi Dutt V <gopiduttv@carestack.com>
2024-11-22 14:01:43 +0530

    ADD partners id

--- src/components/PartnerSection.tsx
+++ src/components/PartnerSection.tsx
@@ -8,7 +8,7 @@ export default function PartnerSection({ partnerList }) {
   const data = useHomeContext()?.homeSettings
   return (
     <Section
-      id="integrations-section"
+      id="partners-section"
       className="py-8 bg-gradient-to-r from-[#8639f8]/50 to-[#f768d1]/50 flex-col justify-start items-center gap-6 "
     >
       <Container className="flex flex-col items-center gap-16">
