31c6895a1d6be6cce4a5b3fe66e335d527c0aabb
Gopi Dutt V <gopiduttv@carestack.com>
2024-11-26 16:41:07 +0530

    ADD new customer heading

--- src/components/CustomerSection.tsx
+++ src/components/CustomerSection.tsx
@@ -4,7 +4,7 @@ import { useHomeContext } from './context/homeSettings'
 import Section from './structure/Section'
 import Container from './structure/Container'
 
-export default function PartnerSection({ partnerList }) {
+export default function CustomerSection({ customers }) {
   const data = useHomeContext()?.homeSettings
   return (
     <Section
@@ -14,16 +14,16 @@ export default function PartnerSection({ partnerList }) {
       <Container className="flex flex-col items-center gap-16">
         <div className="w-full flex flex-col xl:flex-col justify-center items-center gap-10  ">
           <div className="md:w-1/2 text-center font-semibold">
-            Trusted by dental practices across the US.
+            Trusted by multi-location DSOs across the US
           </div>
           <div className="flex flex-col xl:flex-row  gap-8">
-            {partnerList?.map((partner, index) => {
+            {customers?.map((customer, index) => {
               return (
                 <div key={index} className="w-full flex ">
                   <ImageLoader
                     className="flex justify-center h-20 w-72 bg-white rounded-lg "
                     imageClassName="!object-contain h-20 w-auto"
-                    image={partner.image}
+                    image={customer.image}
                     fixed={false}
                   />
                 </div>
